4ef0985bc786e9ed82cc42856eeb753ee9109f1e
Wes Chow <wes.chow@s7labs.com>
2012-02-10 13:50:24 -0500

    Remove Open Sans, add debug flag in templates.

--- src/settings.py
+++ src/settings.py
@@ -126,6 +126,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.static',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
+    'django.core.context_processors.debug',
 )
 
 MIDDLEWARE_CLASSES = (
