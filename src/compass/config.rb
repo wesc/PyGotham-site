198f9e725ffa83efcf7a15af849a0a534b692390
Wes Chow <wes.chow@s7labs.com>
2012-02-22 09:27:39 -0500

    Have compass build css in static/css directory.

--- src/compass/config.rb
+++ src/compass/config.rb
@@ -2,7 +2,7 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "static/css"
+css_dir = "../../static/css"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
